d4ecd18a2ca706349f515014d861670657e66b11
stefan.wasserbauer <stefan.wasserbauer@dynatrace.com>
2019-09-12 21:34:43 +0200

    cleanup

--- app.js
+++ app.js
@@ -2,34 +2,13 @@ let margin = {top: 100, right: 100, bottom: 100, left: 100},
   width = Math.min(700, window.innerWidth - 10) - margin.left - margin.right,
   height = Math.min(width, window.innerHeight - margin.top - margin.bottom - 20);
 
-let data = [
-  [
-    {axis:"CPU",value:0.22},
-    {axis:"Mem",value:0.28},
-    {axis:"Availability",value:0.29},
-    {axis:"Disk space",value:0.17},
-  ],
-  [
-    {axis:"CPU",value:0.81},
-    {axis:"Mem",value:0.4},
-    {axis:"Availability",value:0.6},
-    {axis:"Disk space",value:0.24},
-  ],
-  [
-    {axis:"CPU",value:0.12},
-    {axis:"Mem",value:0.78},
-    {axis:"Availability",value:0.69},
-    {axis:"Disk space",value:0.14},
-  ]
-];
-
 function createRandomData(count) {
 
   function randn_bm() {
     var u = 0, v = 0;
-    while(u === 0) u = Math.random(); //Converting [0,1) to (0,1)
-    while(v === 0) v = Math.random();
-    let num = Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
+    while (u === 0) u = Math.random(); //Converting [0,1) to (0,1)
+    while (v === 0) v = Math.random();
+    let num = Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v);
     num = num / 10.0 + 0.5; // Translate to 0 -> 1
     if (num > 1 || num < 0) return randn_bm(); // resample between 0 and 1
     return num;
@@ -38,14 +17,14 @@ function createRandomData(count) {
   let data = [];
   let counter = (!count) ? 3 : count;
 
-  for(let i = 0; i < counter; i++) {
+  for (let i = 0; i < counter; i++) {
 
     data.push(
       [
-        {axis:"CPU",value:randn_bm()},
-        {axis:"Mem",value:randn_bm()},
-        {axis:"Availability",value:randn_bm()},
-        {axis:"Disk space",value:randn_bm()},
+        {axis: "CPU", value: randn_bm()},
+        {axis: "Mem", value: randn_bm()},
+        {axis: "Availability", value: randn_bm()},
+        {axis: "Disk space", value: randn_bm()},
       ]
     );
 
@@ -54,8 +33,8 @@ function createRandomData(count) {
 
 }
 
-let color = d3.scaleLinear().domain([0,1])
-  .range(["rgba(0,0,0,0)","rgba(0,0,0,1)"]);
+let color = d3.scaleLinear().domain([0, 1])
+  .range(["rgba(0,0,0,0)", "rgba(0,0,0,1)"]);
 
 let radarChartOptions = {
   w: width,
@@ -69,7 +48,6 @@ let radarChartOptions = {
 
 
 let randomData = createRandomData(30);
-console.log(randomData);
 
 //Call function to draw the Radar chart
 RadarChart(".radarChart", randomData, radarChartOptions);
--- radarChart.js
+++ radarChart.js
@@ -1,6 +1,6 @@
 
 function RadarChart(id, data, options) {
-  var cfg = {
+  let cfg = {
     w: 600,				//Width of the circle
     h: 600,				//Height of the circle
     margin: {top: 20, right: 20, bottom: 20, left: 20}, //The margins of the SVG
@@ -18,89 +18,50 @@ function RadarChart(id, data, options) {
 
   //Put all of the options into a variable called cfg
   if('undefined' !== typeof options){
-    for(var i in options){
+    for(let i in options){
       if('undefined' !== typeof options[i]){ cfg[i] = options[i]; }
     }//for i
   }//if
 
   //If the supplied maxValue is smaller than the actual one, replace by the max in the data
-  var maxValue = Math.max(cfg.maxValue, d3.max(data, function(i){return d3.max(i.map(function(o){return o.value;}))}));
+  let maxValue = Math.max(cfg.maxValue, d3.max(data, function(i){return d3.max(i.map(function(o){return o.value;}))}));
 
-  var allAxis = (data[0].map(function(i, j){return i.axis})),	//Names of each axis
+  let allAxis = (data[0].map(function(i, j){return i.axis})),	//Names of each axis
     total = allAxis.length,					//The number of different axes
     radius = Math.min(cfg.w/2, cfg.h/2), 	//Radius of the outermost circle
     Format = d3.format('%'),			 	//Percentage formatting
     angleSlice = Math.PI * 2 / total;		//The width in radians of each "slice"
 
   //Scale for the radius
-  var rScale = d3.scaleLinear()
+  let rScale = d3.scaleLinear()
     .range([0, radius])
     .domain([0, maxValue]);
-
-  /////////////////////////////////////////////////////////
-  //////////// Create the container SVG and g /////////////
-  /////////////////////////////////////////////////////////
 
   //Remove whatever chart with the same id/class was present before
   d3.select(id).select("svg").remove();
 
   //Initiate the radar chart SVG
-  var svg = d3.select(id).append("svg")
+  let svg = d3.select(id).append("svg")
     .attr("width",  cfg.w + cfg.margin.left + cfg.margin.right)
     .attr("height", cfg.h + cfg.margin.top + cfg.margin.bottom)
     .attr("class", "radar"+id);
   //Append a g element
-  var g = svg.append("g")
+  let g = svg.append("g")
     .attr("transform", "translate(" + (cfg.w/2 + cfg.margin.left) + "," + (cfg.h/2 + cfg.margin.top) + ")");
 
-  /////////////////////////////////////////////////////////
-  ////////// Glow filter for some extra pizzazz ///////////
-  /////////////////////////////////////////////////////////
-
   //Filter for the outside glow
-  var filter = g.append('defs').append('filter').attr('id','glow'),
+  let filter = g.append('defs').append('filter').attr('id','glow'),
     feGaussianBlur = filter.append('feGaussianBlur').attr('stdDeviation','2.5').attr('result','coloredBlur'),
     feMerge = filter.append('feMerge'),
     feMergeNode_1 = feMerge.append('feMergeNode').attr('in','coloredBlur'),
     feMergeNode_2 = feMerge.append('feMergeNode').attr('in','SourceGraphic');
 
-  /////////////////////////////////////////////////////////
-  /////////////// Draw the Circular grid //////////////////
-  /////////////////////////////////////////////////////////
 
   //Wrapper for the grid & axes
-  var axisGrid = g.append("g").attr("class", "axisWrapper");
-
-  //Draw the background circles
-  // axisGrid.selectAll(".levels")
-  //   .data(d3.range(1,(cfg.levels+1)).reverse())
-  //   .enter()
-  //   .append("circle")
-  //   .attr("class", "gridCircle")
-  //   .attr("r", function(d, i){return radius/cfg.levels*d;})
-  //   .style("fill", "#CDCDCD")
-  //   .style("stroke", "#CDCDCD")
-  //   .style("fill-opacity", cfg.opacityCircles)
-  //   .style("filter" , "url(#glow)");
-
-  //Text indicating at what % each level is
-  // axisGrid.selectAll(".axisLabel")
-  //   .data(d3.range(1,(cfg.levels+1)).reverse())
-  //   .enter().append("text")
-  //   .attr("class", "axisLabel")
-  //   .attr("x", 4)
-  //   .attr("y", function(d){return -d*radius/cfg.levels;})
-  //   .attr("dy", "0.4em")
-  //   .style("font-size", "10px")
-  //   .attr("fill", "#737373")
-  //   .text(function(d,i) { return Format(maxValue * d/cfg.levels); });
-
-  /////////////////////////////////////////////////////////
-  //////////////////// Draw the axes //////////////////////
-  /////////////////////////////////////////////////////////
+  let axisGrid = g.append("g").attr("class", "axisWrapper");
 
   //Create the straight lines radiating outward from the center
-  var axis = axisGrid.selectAll(".axis")
+  let axis = axisGrid.selectAll(".axis")
     .data(allAxis)
     .enter()
     .append("g")
@@ -131,39 +92,15 @@ function RadarChart(id, data, options) {
   /////////////////////////////////////////////////////////
 
   //The radial line function
-  var radarLine = d3.radialLine()
+  let radarLine = d3.radialLine()
     .radius(function(d) { return rScale(d.value); })
     .angle(function(d,i) {	return i*angleSlice; });
 
   //Create a wrapper for the blobs
-  var blobWrapper = g.selectAll(".radarWrapper")
+  let blobWrapper = g.selectAll(".radarWrapper")
     .data(data)
     .enter().append("g")
     .attr("class", "radarWrapper");
-
-  //Append the backgrounds
-  // blobWrapper
-  //   .append("path")
-  //   .attr("class", "radarArea")
-  //   .attr("d", function(d,i) { return radarLine(d); })
-  //   .style("fill", function(d,i) { return cfg.color(i); })
-  //   .style("fill-opacity", cfg.opacityArea)
-  //   .on('mouseover', function (d,i){
-  //     //Dim all blobs
-  //     d3.selectAll(".radarArea")
-  //       .transition().duration(200)
-  //       .style("fill-opacity", 0.1);
-  //     //Bring back the hovered over blob
-  //     d3.select(this)
-  //       .transition().duration(200)
-  //       .style("fill-opacity", 0.7);
-  //   })
-  //   .on('mouseout', function(){
-  //     //Bring back all blobs
-  //     d3.selectAll(".radarArea")
-  //       .transition().duration(200)
-  //       .style("fill-opacity", cfg.opacityArea);
-  //   });
 
   //Create the outlines
   blobWrapper.append("path")
@@ -185,12 +122,8 @@ function RadarChart(id, data, options) {
     .style("fill", function(d,i,j) { return cfg.color(j); })
     .style("fill-opacity", 0.1);
 
-  /////////////////////////////////////////////////////////
-  //////// Append invisible circles for tooltip ///////////
-  /////////////////////////////////////////////////////////
-
   //Wrapper for the invisible circles on top
-  var blobCircleWrapper = g.selectAll(".radarCircleWrapper")
+  let blobCircleWrapper = g.selectAll(".radarCircleWrapper")
     .data(data)
     .enter().append("g")
     .attr("class", "radarCircleWrapper");
@@ -222,19 +155,15 @@ function RadarChart(id, data, options) {
     });
 
   //Set up the small tooltip for when you hover over a circle
-  var tooltip = g.append("text")
+  let tooltip = g.append("text")
     .attr("class", "tooltip")
     .style("opacity", 0);
-
-  /////////////////////////////////////////////////////////
-  /////////////////// Helper Function /////////////////////
-  /////////////////////////////////////////////////////////
 
   //Taken from http://bl.ocks.org/mbostock/7555321
   //Wraps SVG text
   function wrap(text, width) {
     text.each(function() {
-      var text = d3.select(this),
+      let text = d3.select(this),
         words = text.text().split(/\s+/).reverse(),
         word,
         line = [],
